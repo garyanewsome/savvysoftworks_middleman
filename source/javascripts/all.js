60405acc491347cebfcfd8b4300920b4224c9cee
Gary A. Newsome <garyanewsome@gmail.com>
2016-07-06 13:29:10 -0400

    Add Parallax Container

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,2 +1,12 @@
 //= require jquery
 //= require materialize-sprockets
+
+$(".button-collapse").sideNav();
+
+    $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
+
+$(document).ready(function(){
+      $('.parallax').parallax();
+    });
