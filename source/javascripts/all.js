cf76e7b2a40a6970efc93745579a8f51242d5288
Gary A. Newsome <garyanewsome@gmail.com>
2016-07-07 08:41:10 -0400

    Coffee and Code

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,10 +3,26 @@
 
 $(".button-collapse").sideNav();
 
-    $(document).ready(function(){
-      $('.slider').slider({full_width: true});
+$(document).ready(function(){
+  $('.slider').slider({full_width: true});
+  // parallax container
+  $('.parallax').parallax();
+  // init text area
+  $('input#input_text, textarea#textarea1').characterCounter();
+  // collapsible popout - current clients
+  $('.collapsible').collapsible({
+      accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
     });
 
-$(document).ready(function(){
-      $('.parallax').parallax();
-    });
+});
+
+// jQuery Scroll Animations
+$('a[href^="#"]').on('click', function(event) {
+    var target = $(this.getAttribute('href'));
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 500);
+    }
+});
